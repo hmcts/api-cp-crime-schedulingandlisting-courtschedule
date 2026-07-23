f08aae42697abc5afbefcc73600d44bb0f7b7d83
coling01 <colin.greenwood@hmcts.net>
2026-07-23 08:56:41 +0100

    AMP-913: add APIM infrastructure for court schedule API
    Add infrastructure/ Terraform to register the court schedule API with
    the SPS APIM instance. Uses cnp-module-api-mgmt-api and
    cnp-module-api-mgmt-product modules, sourcing the OpenAPI spec from
    src/main/resources/openapi/openapi-spec.yml.

---
+++ infrastructure/apis.tf
@@ -0,0 +1,49 @@
+locals {
+  api_specs = {
+    for api_key, api in var.apis :
+    api_key => yamldecode(file(api.openapi_spec_path))
+  }
+}
+
+module "apis" {
+  for_each = var.apis
+
+  source = "git::https://github.com/hmcts/cnp-module-api-mgmt-api.git?ref=master"
+
+  api_mgmt_rg   = var.api_mgmt_rg
+  api_mgmt_name = var.api_mgmt_name
+  product_id    = module.product.product_id
+
+  revision = each.value.revision
+
+  display_name = coalesce(
+    try(each.value.display_name, null),
+    try(local.api_specs[each.key].info.title, null),
+    each.key
+  )
+
+  name = coalesce(
+    try(each.value.name, null),
+    lower(regexreplace(
+      coalesce(
+        try(local.api_specs[each.key].info.title, null),
+        each.key
+      ),
+      "[^0-9A-Za-z-]",
+      "-"
+    ))
+  )
+
+  path = coalesce(
+    try(each.value.path, null),
+    trim(try(keys(local.api_specs[each.key].paths)[0], each.key), "/")
+  )
+
+  protocols             = each.value.protocols
+  service_url           = each.value.service_url
+  subscription_required = each.value.subscription_required
+  api_type              = each.value.api_type
+
+  swagger_url    = file(each.value.openapi_spec_path)
+  content_format = "openapi"
+}
---
+++ infrastructure/sbox.tfvars
@@ -0,0 +1,20 @@
+api_mgmt_rg   = "rg-sps-platform-sbox"
+api_mgmt_name = "sps-api-mgmt-sbox"
+
+apim_product = {
+  name                          = "cp-crime-schedulingandlisting"
+  subscription_required         = true
+  subscriptions_limit           = 20
+  approval_required             = false
+  published                     = true
+  product_access_control_groups = ["developers", "administrators"]
+  product_policy                = ""
+}
+
+apis = {
+  courtschedule = {
+    openapi_spec_path = "../src/main/resources/openapi/openapi-spec.yml"
+    service_url       = "https://spnl-sbox-apim-int-gw.cpp.nonlive/amp/slc"
+    revision          = "1"
+  }
+}
